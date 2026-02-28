0cd9b049a86602ebb9d209c3a33b84b55df580cb
Aadarsh017 <maddheshiyaaadarsh9@gmail.com>
2026-02-28 12:05:08 +0530

    Update index.js

--- WEB/js4/index.js
+++ WEB/js4/index.js
@@ -3,31 +3,31 @@ let b = document.getElementById("guest");
 
 let count = 0
 function first() {
-    count +=1
+    count += 1
     a.innerText = count
 
 }
 function second() {
-    count +=2
+    count += 2
     a.innerText = count
 }
 
 function third() {
-    count +=3
+    count += 3
     a.innerText = count
 }
 
 function four() {
-    count +=1
+    count += 1
     b.innerText = count
 
 }
 function five() {
-    count +=2
+    count += 2
     b.innerText = count
 }
 
 function six() {
-    count +=3
+    count += 3
     b.innerText = count
 }
